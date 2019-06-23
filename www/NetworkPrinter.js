82d902cbff6802b9536ec05cd901d0faa70d4af6
Saw Maine K <sawmainek90@gmail.com>
2019-06-24 00:06:29 +0630

    Ready to product release

--- www/NetworkPrinter.js
+++ www/NetworkPrinter.js
@@ -1,21 +1,18 @@
-var exec = cordova.require('cordova/exec');
+cordova.define("com.smk.networkprinter.NetworkPrinter", function (require, exports, module) {
+    var exec = cordova.require('cordova/exec');
 
-var NetworkPrinter = function() {
-    
-};
+    var NetworkPrinter = {
+        print: function (option, onSuccess, onError) {
+            var errorCallback = function (obj) {
+                onError(obj);
+            };
 
-NetworkPrinter.prototype.print = function(option, onSuccess, onError) {
-    var errorCallback = function(obj) {
-        onError(obj);
+            var successCallback = function (obj) {
+                onSuccess(obj);
+            };
+
+            exec(successCallback, errorCallback, 'NetworkPrinter', 'print', [option]);
+        }
     };
-
-    var successCallback = function(obj) {
-        onSuccess(obj);
-    };
-
-    exec(successCallback, errorCallback, 'NetworkPrinter', 'print', [option]);
-};
-
-if (typeof module != 'undefined' && module.exports) {
     module.exports = NetworkPrinter;
-}
+});
